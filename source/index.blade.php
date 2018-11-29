56d353ffc1ebfbe50ab20f30111e87501d1479df
Alec Lorraine <alec@brighten.in>
2018-11-29 14:02:21 -0800

    deploy

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,31 +8,24 @@
 
 
       <h3> What is Moodboost? </h3>
-      <p>
+        <p>
           1) Once a day, tap the "moodboost" button. </br>
-          2) Take 5 seconds to read your moodboost (news, story, quote, etc) of the day.</br>
-          3) Feel happier. </p>
+          2) Take 5 seconds to read your good news story of the day.</br>
+          3) Feel happier.
+        </p>
 
-    <h3> How do I cancel my subscription?  </h2>
-      <p> iOS USERS - You can cancel your subscription directly from your iPhone by following these steps: </br>
+      <h3> How do I save a moodboost that I like? </h3>
+        <p>
+          Just tap the heart icon in the top right!
+        </p>
 
-      Subscriptions are administered and managed directly by Apple iTunes.</br>
+      <h3> How do I share a boost on Instagram? </h3>
+        <p>
+          Tap the Share Story button to share the image to social platforms, text it to a friend, or save it to your camera roll!
+        </p>
 
-      1) Access Settings in the home of your device</br>
 
-      2) Scroll down and tap iTunes Store & AppStore</br>
 
-      3) Tap your e-mail and Apple ID, View Apple ID and enter your password</br>
-
-      4) Now, tap Subscriptions and select Moodboost</br>
-
-      5) Tap the "Cancel Subscription" button to cancel</br>
-
-      ANDROID USERS - There are two ways to manage your subscriptions:</br>
-
-      1) On Android, open the Google Play Store, then tap Menu > Subscriptions</br>
-
-      2) On a computer, go to play.google.com, then in the left menu click "My Subscriptions" </p>
 
 
 

60b85fe7223974f7422a08484eeefd4522ee839a
Alec Lorraine <alec@brighten.in>
2019-02-26 14:11:04 -0800

    update copy

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,32 +8,25 @@
 
 
       <h3> What is Moodboost? </h3>
+  <!--       <p> These days it feels like all news is negative and depressing. </br>
+        So Moodboost sends you one <b> positive </b> news story each day. </br>
+      Take 5 seconds to read the story, and feel a bit happier </p> -->
         <p>
-          1) Once a day, tap the moodboost button. </br>
+          1) Once a day, open the moodboost app. </br>
           2) Take 5 seconds to read your good news story of the day.</br>
           3) Feel happier.
         </p>
 
-      <h3> How do I save a moodboost that I like? </h3>
-        <p>
-          Smile at it! Our smile detection technology will detect your smile and save it to your favorites.
-        </p>
 
       <h3> How do I share a boost on Instagram? </h3>
         <p>
           Tap the Share Story button to share the image to social platforms, text it to a friend, or save it to your camera roll!
         </p>
 
-       <h3> What are we doing with your smile data?</h3>
+       <h3> I want to know more about today's story!</h3>
         <p>
-          NOTHING. We’re checking for your smile cause we wanna make sure we’re doing our job right!
+          Tap and hold on any boost to read about the story in more detail.
         </p>
-
-
-
-
-
-
   </div>
 </div>
 

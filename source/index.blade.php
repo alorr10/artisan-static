57abf8955851f4a0c4c75e98ecbac6c075741c34
Alec Lorraine <alec@brighten.in>
2020-04-10 10:51:21 -0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,19 +11,10 @@
         <p>
           Every morning, you'll receive the most interesting positive news story we could find. It's a quick daily dose of positivity.
         </p>
-       <h3> How do I view the whole article?</h3>
-        <p>
-          Touch + hold on a story.
-        </p>
 
         <h3> I heard it can magically detect when I smile?</h3>
         <p>
           Impossible. Or is it? Go to settings to turn it on.
-        </p>
-
-        <h3>It costs money after the free trial? B*tch I'm broke!</h3>
-        <p>
-         You might be $2.99 poorer next month. But hey at least you'll be in a good mood.
         </p>
 
         <h3> How do I share a boost on Instagram? </h3>
